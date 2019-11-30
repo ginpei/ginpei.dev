26995352eb229bb3f79ec3d9b9cd45c731922f97
ginpei <ginpei@gmail.com>
2019-11-30 11:09:56 +0900

    extract sections

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -2,6 +2,87 @@ import React from 'react';
 
 const SlashDelimiter: React.FC = () => <> / </>;
 
+const PersonalInformationSection: React.FC = () => (
+  <section>
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <td>
+            Ginpei Takanashi
+            /
+            高梨ギンペイ
+          </td>
+        </tr>
+        <tr>
+          <th>GitHub</th>
+          <td>
+            <a href="https://github.com/ginpei/">@ginpei</a>
+          </td>
+        </tr>
+        <tr>
+          <th>Twitter</th>
+          <td>
+            <a href="https://twitter.com/ginpei_en">@ginpei_en</a>
+            {' / '}
+            <a href="https://twitter.com/ginpei_jp">@ginpei_jp</a>
+          </td>
+        </tr>
+        <tr>
+          <th>Mail</th>
+          <td>
+            ginpei@gmail.com
+          </td>
+        </tr>
+        <tr>
+          <th>Posts</th>
+          <td>
+            <a href="https://ginpen.com/">Ginpen.com （日本語）</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+);
+
+const AppSection: React.FC = () => (
+  <section>
+    <h2>Apps</h2>
+    <ul>
+      <li>
+        <a href="https://chrome.google.com/webstore/detail/devtools-z-index/bcnpmhefiohkpmjacfoanhbjhikegmoe">DevTools z-index for Chrome</a>
+        <SlashDelimiter />
+        <a href="https://addons.mozilla.org/en-US/firefox/addon/devtools-z-index/">for Firefox</a>
+        <SlashDelimiter />
+        <a href="https://github.com/ginpei/devtools-z-index">GitHub</a>
+      </li>
+      <li>
+        <a href="https://chrome.google.com/webstore/detail/%E3%81%8A%E5%89%8D%E3%82%92%E6%B6%88%E3%81%99%E6%96%B9%E6%B3%95-for-github/lhicnokhbnhbecjiahdaokfphfhjkbnp">お前を消す方法 for GitHub for Chrome</a>
+        <SlashDelimiter />
+        <a href="https://addons.mozilla.org/en-US/firefox/addon/%E3%81%8A%E5%89%8D%E3%82%92%E6%B6%88%E3%81%99%E6%96%B9%E6%B3%95-for-github/">for Firefox</a>
+        <SlashDelimiter />
+        <a href="https://github.com/ginpei/omae-wo-kesu-houhou-for-github">GitHub</a>
+      </li>
+      <li>
+        <a href="https://pretty-letters.ginpei.info/">Pretty Letters</a>
+        <SlashDelimiter />
+        <a href="https://github.com/ginpei/pretty-letters">GitHub</a>
+      </li>
+      <li>
+        <a href="https://understanding-transform-matrix.ginpei.info/">Understanding transform:matrix()</a>
+        <SlashDelimiter />
+        <a href="https://github.com/ginpei/understanding-transform-matrix">GitHub</a>
+      </li>
+      <li>
+        <a href="https://potoshop.ginpei.info/">Potoshop (Beta)</a>
+      </li>
+      <li>
+        <a href="https://ccc.ginpei.info/">Code Chat Cat (beta)</a>
+      </li>
+    </ul>
+  </section>
+);
+
 const PublicationSection: React.FC = () => (
   <section>
     <h1>Paper Publications</h1>
@@ -37,77 +118,8 @@ const HomePage: React.FC = () => {
             width="512"
           />
         </div>
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <td>
-                Ginpei Takanashi
-                /
-                高梨ギンペイ
-              </td>
-            </tr>
-            <tr>
-              <th>GitHub</th>
-              <td>
-                <a href="https://github.com/ginpei/">@ginpei</a>
-              </td>
-            </tr>
-            <tr>
-              <th>Twitter</th>
-              <td>
-                <a href="https://twitter.com/ginpei_en">@ginpei_en</a>
-                {' / '}
-                <a href="https://twitter.com/ginpei_jp">@ginpei_jp</a>
-              </td>
-            </tr>
-            <tr>
-              <th>Mail</th>
-              <td>
-                ginpei@gmail.com
-              </td>
-            </tr>
-            <tr>
-              <th>Posts</th>
-              <td>
-                <a href="https://ginpen.com/">Ginpen.com （日本語）</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <h2>Apps</h2>
-        <ul>
-          <li>
-            <a href="https://chrome.google.com/webstore/detail/devtools-z-index/bcnpmhefiohkpmjacfoanhbjhikegmoe">DevTools z-index for Chrome</a>
-            <SlashDelimiter />
-            <a href="https://addons.mozilla.org/en-US/firefox/addon/devtools-z-index/">for Firefox</a>
-            <SlashDelimiter />
-            <a href="https://github.com/ginpei/devtools-z-index">GitHub</a>
-          </li>
-          <li>
-            <a href="https://chrome.google.com/webstore/detail/%E3%81%8A%E5%89%8D%E3%82%92%E6%B6%88%E3%81%99%E6%96%B9%E6%B3%95-for-github/lhicnokhbnhbecjiahdaokfphfhjkbnp">お前を消す方法 for GitHub for Chrome</a>
-            <SlashDelimiter />
-            <a href="https://addons.mozilla.org/en-US/firefox/addon/%E3%81%8A%E5%89%8D%E3%82%92%E6%B6%88%E3%81%99%E6%96%B9%E6%B3%95-for-github/">for Firefox</a>
-            <SlashDelimiter />
-            <a href="https://github.com/ginpei/omae-wo-kesu-houhou-for-github">GitHub</a>
-          </li>
-          <li>
-            <a href="https://pretty-letters.ginpei.info/">Pretty Letters</a>
-            <SlashDelimiter />
-            <a href="https://github.com/ginpei/pretty-letters">GitHub</a>
-          </li>
-          <li>
-            <a href="https://understanding-transform-matrix.ginpei.info/">Understanding transform:matrix()</a>
-            <SlashDelimiter />
-            <a href="https://github.com/ginpei/understanding-transform-matrix">GitHub</a>
-          </li>
-          <li>
-            <a href="https://potoshop.ginpei.info/">Potoshop (Beta)</a>
-          </li>
-          <li>
-            <a href="https://ccc.ginpei.info/">Code Chat Cat (beta)</a>
-          </li>
-        </ul>
+        <PersonalInformationSection />
+        <AppSection />
         <PublicationSection />
       </div>
     </div>
